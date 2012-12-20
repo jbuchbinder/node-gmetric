ac2f6a10995efdc4eff7e915566cad63425ed762
Josh Toft <joshtoft@gmail.com>
2012-12-20 11:59:13 -0800

    Fixed string packing

--- test/commands.js
+++ test/commands.js
@@ -4,7 +4,8 @@ describe('commands', function() {
   it("be able to properly pack integers", function(done){
     var buffer = new Buffer(11);
     var pos = buffer.write('awesome');
-    cmds.pack_int(buffer, 33, pos);
+    var int_pos = cmds.pack_int(buffer, 33, pos);
+    int_pos.should.equal(11);
     buffer.toString('utf-8', 0, 7).should.equal("awesome");
     buffer.readInt32BE(7).should.equal(33);
     cmds.pack_int(buffer, 47, pos);
@@ -26,11 +27,24 @@ describe('commands', function() {
   it("should be able to properly pack booleans", function(done){
     var buffer = new Buffer(11);
     var pos = buffer.write('awesome');
-    cmds.pack_bool(buffer, true, pos);
+    var int_pos = cmds.pack_bool(buffer, true, pos);
     buffer.toString('utf-8', 0, 7).should.equal("awesome");
+    int_pos.should.equal(11);
     buffer.readInt32BE(7).should.equal(1);
     cmds.pack_bool(buffer, false, pos);
     buffer.readInt32BE(7).should.equal(0);
+    done();
+  });
+
+  it("should be able to properly pack xdr strings", function(done){
+    var teststring = "thebeststring";
+    var buffer = new Buffer(20);
+    cmds.pack_string(buffer, teststring, 0);
+    buffer.readInt32BE(0).should.equal(teststring.length);
+    buffer.toString('utf-8', 4, 17).should.equal(teststring);
+    buffer[17].should.equal(0);
+    buffer[18].should.equal(0);
+    buffer[19].should.equal(0);
     done();
   });
 });
